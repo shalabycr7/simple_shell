f96f123feb3ad1db976feeaf6a68c6faabc3f470
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-05-20 22:09:31 +0300

    Created aux_str.c file.

---
+++ aux_str.c
@@ -0,0 +1,106 @@
+#include "main.h"
+
+/**
+ * _strcat - concate strings.
+ * @dest: dest pointer to copied str.
+ * @src: src pointer to str.
+ * Return: destination.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * strcpy - copie string pointed to by src.
+ * @dest: dest pointer to copied str.
+ * @src: src pointer to str.
+ * Return: destination.
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	size_t a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
+/**
+ * _strcmp - compares two strings.
+ * @s1: str compared.
+ * @s2: str compared.
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+/**
+ * _strchr - find a char in string.
+ * @s: string.
+ * @c: character.
+ * Return: pointer to char location.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
+/**
+ * _strspn - find substring length.
+ * @s: initial piece.
+ * @accept: accepted bytes.
+ * Return: number of bytes.
+ */
+int _strspn(char *s, char *accept)
+{
+	int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
