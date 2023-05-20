639b2c1329ae218a8d84577f00f00edddd030682
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-05-20 20:51:52 +0300

    Created aux_mem.c filr.

---
+++ aux_mem.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies information to void pointers.
+ * @newptr: pointer dest.
+ * @ptr: pointer src.
+ * @size: pointer size.
+ *
+ * Return: no return.
+ */
+void _memcpy(void *newptr, const void *ptr, unsigned int size)
+{
+	char *char_ptr = (char *)ptr;
+	char *char_newptr = (char *)newptr;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		char_newptr[i] = char_ptr[i];
+}
+
+/**
+ * _realloc - book a block.
+ * @ptr: pointer to previously pointed.
+ * @old_size: sizer.
+ * @new_size: new size.
+ * Return: ptr.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	if (new_size < old_size)
+		_memcpy(newptr, ptr, new_size);
+	else
+		_memcpy(newptr, ptr, old_size);
+
+	free(ptr);
+	return (newptr);
+}
+
+/**
+ * _reallocdp - book a memory block.
+ * @ptr: double pointer fo memory.
+ * @old_size: size.
+ * @new_size: new size.
+ * Return: ptr.
+ */
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
+{
+	char **newptr;
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(sizeof(char *) * new_size));
+
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(sizeof(char *) * new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		newptr[i] = ptr[i];
+
+	free(ptr);
+
+	return (newptr);
+}
