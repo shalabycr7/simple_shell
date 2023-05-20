9d289b642bef426598addd44ec27728b8fa081a4
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-05-20 19:50:57 +0300

    Created aux_lists.c file.

---
+++ aux_lists.c
@@ -0,0 +1,109 @@
+#include "main.h"
+
+/**
+ * add_sep_node_end - adds separator at the end.
+ * @head: linked list head.
+ * @sep: separator found.
+ * Return: head address.
+ */
+sep_list *add_sep_node_end(sep_list **head, char sep)
+{
+	sep_list *new, *temp;
+
+	new = malloc(sizeof(sep_list));
+	if (new == NULL)
+		return (NULL);
+
+	new->separator = sep;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_sep_list - free sep_list.
+ * @head: linked list head.
+ * Return: no value.
+ */
+void free_sep_list(sep_list **head)
+{
+	sep_list *temp;
+	sep_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * add_line_node_end - put command line at the end.
+ * @head: linked list head.
+ * @line: command line.
+ * Return: head address.
+ */
+line_list *add_line_node_end(line_list **head, char *line)
+{
+	line_list *new, *temp;
+
+	new = malloc(sizeof(line_list));
+	if (new == NULL)
+		return (NULL);
+
+	new->line = line;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_line_list - free line_list.
+ * @head: linked list head.
+ * Return: no value.
+ */
+void free_line_list(line_list **head)
+{
+	line_list *temp;
+	line_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
