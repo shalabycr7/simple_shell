e6011d8216990e2081e056cf692aea23ce4a0462
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-05-20 21:22:39 +0300

    Created aux_stdlib.c file.

---
+++ aux_stdlib.c
@@ -0,0 +1,99 @@
+#include "main.h"
+
+/**
+ * get_len - find lenght of a given number.
+ * @n:  given number.
+ * Return: number length.
+ */
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
+/**
+ * aux_itoa - convert to string.
+ * @n: given number.
+ * Return: converted string.
+ */
+char *aux_itoa(int n)
+{
+	unsigned int n1;
+	int lenght = get_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	lenght--;
+	do
+	{
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	} while (n1 > 0);
+	return (buffer);
+}
+
+/**
+ * _atoi - converts to integer.
+ * @s: given string.
+ * Return: integer number.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
