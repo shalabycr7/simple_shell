9c0bc7eefd42302c161de78fa54959b19730e594
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-05-20 20:12:41 +0300

    Created aux_lists2.c file.

---
+++ aux_lists2.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * add_rvar_node - add variable to end.
+ * @head: linked list head.
+ * @lvar: variable length.
+ * @val: variable value.
+ * @lval: value length.
+ * Return: head address.
+ */
+r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
+{
+	r_var *new, *temp;
+
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
+
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_rvar_list - free r_var list.
+ * @head: linked list head.
+ * Return: no value.
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
