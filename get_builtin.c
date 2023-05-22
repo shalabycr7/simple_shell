43f7c58081e673545fa9c6a55b60be005c36811c
Ghada3333 <125550068+Ghada3333@users.noreply.github.com>
2023-05-22 20:39:15 +0300

    Created get_builtin.c file

---
+++ get_builtin.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_builtin - function that pass the command arg.
+ * @cmd: command givin
+ * Return: pointer of the command.
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{"env", _env},
+		{"exit", exit_shell},
+		{"setenv", _setenv},
+		{"unsetenv", _unsetenv},
+		{"cd", cd_shell},
+		{"help", get_help},
+		{NULL, NULL}};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
