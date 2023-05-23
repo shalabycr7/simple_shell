7fd82d412345f3cb47270dedec6b14c68af8dd5a
Ghada3333 <125550068+Ghada3333@users.noreply.github.com>
2023-05-23 10:06:59 +0300

    Created read_line.c file.

---
+++ read_line.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * read_line - get the input.
+ * @i_eof: return getline value.
+ * Return: input string.
+ */
+char *read_line(int *i_eof)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&input, &bufsize, stdin);
+
+	return (input);
+}
